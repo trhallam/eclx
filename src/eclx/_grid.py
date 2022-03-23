054bf6ac429c5c829b706fc0dacd0a9da4f3a03e
Tony Hallam <trhallam@gmail.com>
2022-03-23 10:29:03 +0000

    add get sim surface

--- src/eclx/_grid.py
+++ src/eclx/_grid.py
@@ -1,10 +1,14 @@
 """Load Eclipse Grid Files
 """
+from typing import Literal
 import pathlib
 import itertools
 import contextlib
+from enum import Enum
 
 import pandas as pd
+import numpy as np
+from numpy import typing as npt
 
 from ecl.grid import EclGrid
 
@@ -15,27 +19,47 @@ tqdm = import_tqdm()
 GRID_FILE_TYPES = [EclFileEnum.ECL_GRID_FILE, EclFileEnum.ECL_EGRID_FILE]
 
 
-def _corner_names():
+class EclGridCPrefix(Enum):
+    x = "x"
+    y = "y"
+    z = "z"
+
+
+class EclSimFaces(Enum):
+    i1 = (0, 4, 6, 2)
+    i2 = (1, 5, 7, 3)
+    j1 = (0, 1, 5, 4)
+    j2 = (2, 3, 7, 6)
+    k1 = (0, 1, 3, 2)
+    k2 = (4, 5, 7, 6)
+    ecl_order = (0, 1, 2, 3, 4, 5, 6, 7)
+
+
+def _corner_names(order=EclSimFaces.ecl_order.value):
     """create corner names list so they can be easily referenced later"""
     return [
         f"{dim}{n}"
         for (n, dim) in itertools.product(
-            range(8),
-            ("x", "y", "z"),
+            order,
+            (
+                EclGridCPrefix["x"].value,
+                EclGridCPrefix["y"].value,
+                EclGridCPrefix["z"].value,
+            ),
         )
     ]
 
 
 def _xcorn_names():
-    return [c for c in _corner_names() if "x" in c]
+    return [c for c in _corner_names() if EclGridCPrefix["x"].value in c]
 
 
 def _ycorn_names():
-    return [c for c in _corner_names() if "y" in c]
+    return [c for c in _corner_names() if EclGridCPrefix["y"].value in c]
 
 
 def _zcorn_names():
-    return [c for c in _corner_names() if "z" in c]
+    return [c for c in _corner_names() if EclGridCPrefix["z"].value in c]
 
 
 @contextlib.contextmanager
@@ -53,10 +77,39 @@ def open_EclGrid(filepath):
             raise ValueError
         egrid = EclGrid.load_from_file(str(filepath.absolute()))
         yield egrid
-    except (ValueError, OSError): # OSError on windows
+    except (ValueError, OSError):  # OSError on windows
         raise ValueError(f"cannot interpret file type {filepath}")
     finally:
         del egrid
+
+
+def get_face_corner_names(face: EclSimFaces):
+    face_ns = EclSimFaces[face].value
+    return _corner_names(order=face_ns)
+
+
+def get_sim_surface(
+    xyzcorn: pd.DataFrame,
+    n: int,
+    face: Literal["top", "base"] = "top",
+    slice_dir: Literal["i", "j", "k"] = "k",
+) -> npt.NDArray(3, np.float_):
+    """Get the corner points which define a surface from the grid using an i,j or k index `n`.
+
+    Args:
+        xyzcorn: The corner point grid dataframe from EclDeck
+        n: The i/j/k layer to slice
+        face: The top or base face of a cell
+        slice_dir: The direction to slice along
+    """
+    face = 1 if face == "top" else 2
+    facename = f"{slice_dir}{face}"
+    corners = xyzcorn.query(f"{slice_dir} == {n}")
+    if corners.empty:
+        raise ValueError(f"No cells match {slice_dir} == {n}")
+    corners = corners[get_face_corner_names(facename)].values.reshape(-1, 3)
+    corners = np.unique(corners, axis=0)
+    return corners
 
 
 def get_ecl_grid_dims(filepath):
